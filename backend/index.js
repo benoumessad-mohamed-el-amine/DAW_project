0e62250423d93c9ace1b5c64f7394cfcd94c0d6f
yutogx04 <lehanine.faical@gmail.com>
2025-04-21 04:39:40 -0400

    lol

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,55 @@
-const examRoutes = require('./routes/exam.routes');
+/* const examRoutes = require('./routes/exam.routes');
 app.use('/api/exams', examRoutes);
 
 const submissionRoutes = require('./routes/submission.routes');
-app.use('/api', submissionRoutes);
+app.use('/api', submissionRoutes); */
+
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "examWebsite"
+})
+connection.connect()
+
+app = express();
+app.use(cors());
+
+/* app.get('/auth/users', (req, res) => {
+    connection.query('SELECT * FROM students', (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error retrieving users');
+        } else {
+            res.json(result);
+        }
+    })
+}); */
+app.get('/auth/users',(req,res)=>{
+    connection.query(`SELECT * FROM students WHERE _id=${req.id}`,(err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error retrieving users');
+        } else {
+            res.json(result);
+        }
+
+})
+})
+app.post('/auth/signin', (req, res) => {
+    console.log(req.body);
+});
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
+
+//get all users from database
+axios.get('http://localhost:3000/auth/users').then((response) => {
+  //users contain all the users
+  const users=response.data;
+  console.log(users)
+
+  //adding it the first user the bottom of the page as an example
+  const nameLi = document.getElementById("name");
+  nameLi.append(`${users[0].name}`)
+  const emailLi = document.getElementById("email");
+  emailLi.append(`${users[0].email}`)
+  const groupLi = document.getElementById("group");
+  groupLi.append(`group : ${users[0].grp}`)
+  const sectionLi = document.getElementById("section");
+  sectionLi.append(`section : ${users[0].section}`)
+})
+
+
 function authenticate() {
     const studentId = document.getElementById("student_id").value;
     const password = document.getElementById("password").value;
     const errorMsg = document.getElementById("error-msg");
-  
+
     const validStudentId = "student123";
     const validPassword = "password123";
-  
+
+
     // Authentication logic
     if (studentId === validStudentId && password === validPassword) {
-      window.location.href = "/main_page.html";  
+      window.location.href = "/main_page.html";
     } else {
       errorMsg.textContent = "Incorrect student Id or password.";
     }
   }
-  
